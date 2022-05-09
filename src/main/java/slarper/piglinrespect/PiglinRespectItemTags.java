30fe78155bcfaa14d9caa8046190f3215c268e32
SpaceTeleport <naivesimple@outlook.com>
2022-05-09 23:10:27 +0800

    version: 0.3.0 -> 0.4.0 add feature: piglin respects netherite armors.

---
+++ src/main/java/slarper/piglinrespect/PiglinRespectItemTags.java
@@ -0,0 +1,10 @@
+package slarper.piglinrespect;
+
+import net.minecraft.item.Item;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class PiglinRespectItemTags {
+    public static final TagKey<Item> PIGLIN_RESPECT_ITEMS = TagKey.of(Registry.ITEM_KEY, new Identifier("piglinrespect","piglin_respect_items"));
+}
---
+++ src/main/java/slarper/piglinrespect/mixin/PiglinBrainMixin.java
@@ -0,0 +1,31 @@
+package slarper.piglinrespect.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.PiglinBrain;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import slarper.piglinrespect.PiglinRespectItemTags;
+
+import java.util.Iterator;
+
+@Mixin(PiglinBrain.class)
+public class PiglinBrainMixin {
+    @Inject(
+            method = "wearsGoldArmor",
+            at = @At(
+                    value = "INVOKE_ASSIGN",
+                    target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"
+            ),
+            cancellable = true,
+            locals = LocalCapture.CAPTURE_FAILSOFT
+    )
+    private static void isInTag(LivingEntity entity, CallbackInfoReturnable<Boolean> cir, Iterable<ItemStack> iterable, Iterator<ItemStack> var2, ItemStack itemStack){
+        if (itemStack.isIn(PiglinRespectItemTags.PIGLIN_RESPECT_ITEMS)){
+            cir.setReturnValue(true);
+        }
+    }
+}
