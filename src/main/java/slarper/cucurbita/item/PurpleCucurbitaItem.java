6f4d3b6ba644e3ae0f59fa40a7f6c33bb021139c
Slarper <naivesimple@outlook.com>
2022-06-05 12:18:04 +0800

    version: 0.10.7.0 log: 1.fixed : the cucurbita is easy to spawn Suffocated mobs. Now it works like spawn eggs.

--- src/main/java/slarper/cucurbita/item/PurpleCucurbitaItem.java
+++ src/main/java/slarper/cucurbita/item/PurpleCucurbitaItem.java
@@ -1,14 +1,20 @@
 package slarper.cucurbita.item;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.sound.SoundCategory;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
-import net.minecraft.world.World;
-import slarper.cucurbita.Sounds;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+import slarper.cucurbita.mixin.EntityTypeAccessor;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -20,34 +26,55 @@ public class PurpleCucurbitaItem extends Item {
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
+        if (!(context.getWorld() instanceof ServerWorld world)) {
+            return ActionResult.SUCCESS;
+        } else {
+            ItemStack itemStack = context.getStack();
+            BlockPos blockPos = context.getBlockPos();
+            Direction direction = context.getSide();
+            BlockState blockState = world.getBlockState(blockPos);
+            PlayerEntity player = context.getPlayer();
+            NbtCompound itemNbt = itemStack.getNbt();
 
-        // swing hand
-        if (context.getPlayer() != null){
-            context.getPlayer().swingHand(context.getHand());
+            BlockPos pos;
+            if (blockState.getCollisionShape(world, blockPos).isEmpty()) {
+                pos = blockPos;
+            } else {
+                pos = blockPos.offset(direction);
+            }
+
+            EntityType<?> entityType = this.getEntityType(itemNbt);
+            if (entityType == null){
+                return ActionResult.FAIL;
+            }
+
+            Entity entity = entityType.create(world);
+            if (entity == null) {
+                return ActionResult.CONSUME;
+            }
+            EntityType.loadFromEntityNbt(world,player,entity,itemNbt);
+            entity.setPosition((double)pos.getX() + 0.5D, pos.getY() + 1, (double)pos.getZ() + 0.5D);
+            boolean invertY = !Objects.equals(blockPos, pos) && direction == Direction.UP;
+            double d = EntityTypeAccessor.invokeGetOriginY(world, pos, invertY, entity.getBoundingBox());
+            entity.refreshPositionAndAngles((double)pos.getX() + 0.5D, (double)pos.getY() + d, (double)pos.getZ() + 0.5D, entity.getYaw(), entity.getPitch());
+            world.spawnEntityAndPassengers(entity);
+            if (player!=null){
+                player.setStackInHand(context.getHand(),new ItemStack(Items.COPPER_CUCURBITA));
+            }
+            world.emitGameEvent(player, GameEvent.ENTITY_PLACE, blockPos);
+            return ActionResult.CONSUME;
         }
+    }
 
-        // try to reveal mob from nbt
-        Optional<Entity> optional = EntityType.getEntityFromNbt(context.getStack().getOrCreateSubNbt(EntityType.ENTITY_TAG_KEY),context.getWorld());
-        if (optional.isPresent()){
-            Entity entity = optional.get();
-            entity.setPosition(context.getHitPos());
-            context.getWorld().spawnEntity(entity);
-            ItemStack calabash = new ItemStack(Items.COPPER_CUCURBITA);
-            Objects.requireNonNull(context.getPlayer()).setStackInHand(context.getHand(),calabash);
-
-            World world = context.getWorld();
-            if (!world.isClient) {
-                world.playSound(
-                        null, // Player - if non-null, will play sound for every nearby player *except* the specified player
-                        entity.getBlockPos(), // The position of where the sound will come from
-                        Sounds.OPEN_BOTTLE_EVENT, // The sound that will play
-                        SoundCategory.PLAYERS, // This determines which of the volume sliders affect this sound
-                        1f, //Volume multiplier, 1 is normal, 0.5 is half volume, etc
-                        1f // Pitch multiplier, 1 is normal, 0.5 is half pitch, etc
-                );
+    @Nullable
+    public EntityType<?> getEntityType(@Nullable NbtCompound nbt) {
+        if (nbt != null && nbt.contains("EntityTag", 10)) {
+            NbtCompound nbtCompound = nbt.getCompound("EntityTag");
+            if (nbtCompound.contains("id", 8)) {
+                Optional<EntityType<?>> entityType = EntityType.get(nbtCompound.getString("id"));
+                return entityType.orElse(null);
             }
         }
-
-        return super.useOnBlock(context);
+        return null;
     }
 }
