507ea91127a441a5438d6b2910edcc4ed3fff3a6
Slarper <naivesimple@outlook.com>
2022-06-06 17:41:01 +0800

    version: 0.10.9.3 log: 1.optimize code.

--- src/main/java/slarper/transcendingtrident/mixin/TridentEntityMixin.java
+++ src/main/java/slarper/transcendingtrident/mixin/TridentEntityMixin.java
@@ -1,25 +1,12 @@
 package slarper.transcendingtrident.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LightningEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.TridentEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import slarper.transcendingtrident.Functions;
 
 @Mixin(TridentEntity.class)
